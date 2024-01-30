3fd4928150dd953f283c9f1c748339c529476afb
Ricky Fleming <raflemin@gmail.com>
2024-01-30 11:52:10 -0800

    current_ratio function

--- finance_formulas.py
+++ finance_formulas.py
@@ -10,3 +10,10 @@ def accounting_equation(assets:float, liabilities:float, shareholder_equity:floa
     if not balanced:
         return assets - liabilities - shareholder_equity
     return balanced
+
+def current_ratio(current_assets: float, current_liabilities:float) -> float:
+    """
+    Calculates if company can pay its current bills.
+    In general, above 1.5 is safe and below 1.5 is risky
+    """
+    return current_assets / current_liabilities
