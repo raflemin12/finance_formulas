b498d5cf8080bad2cb38332d2dfce1e58746b3c0
Ricky Fleming <raflemin@gmail.com>
2024-01-30 11:46:30 -0800

    create accounting_equation function

---
+++ finance_formulas.py
@@ -0,0 +1,13 @@
+
+
+def accounting_equation(assets:float, liabilities:float, shareholder_equity:float) -> bool:
+    """Assets = Libabilities + Shareholders Equity
+    Returns true when accounting equation is balanced
+    Returns the difference when accounting equation is not balanced
+    """
+    balanced = assets == liabilities + shareholder_equity
+
+    if not balanced:
+        return assets - liabilities - shareholder_equity
+    else:
+        return balanced
